b6829bb7bb6a6137d651e2055242f78413cdfeee
sinon2002 <153992175+sinon2002@users.noreply.github.com>
2026-09-22 23:00:31 +0600

    Update layout and styling in EventsCerts component
    Changed layout to center items and adjusted mobile photo width.

--- src/components/landing/EventsCerts.tsx
+++ src/components/landing/EventsCerts.tsx
@@ -82,7 +82,9 @@ export function Events() {
         <Reveal className="overflow-hidden rounded-4xl bg-card shadow-soft p-6 md:p-0 md:flex items-center">
           
           {/* ОБЩИЙ КОНТЕЙНЕР ДЛЯ МОБИЛОК: изменили items-start на items-center для выравнивания по центру */}
-        <div className="flex items-center gap-4 md:block md:w-[35%] md:flex-shrink-0 md:p-6 scale-125 origin-center sm:scale-100">
+     <div className="flex flex-col md:flex-col-reverse items-center justify-center gap-6 md:block md:w-[35%] md:flex-shrink-0 md:p-6 scale-125 origin-center sm:scale-100">
+
+
 
 
             
